e8e3f24a42926dc277dcc34f2bcdf0ad38305ecd
Tu Nombre <montespierre@gmail.com>
2020-12-19 17:46:48 -0500

    hola spring

---
+++ src/main/java/com/weimont/app/AgendaIncaApplication.java
@@ -0,0 +1,13 @@
+package com.weimont.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaIncaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaIncaApplication.class, args);
+	}
+
+}
